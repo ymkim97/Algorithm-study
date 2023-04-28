dee637b986080a78ac108bac5b03e98e02c29911
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2023-04-28 19:52:22 +0900

    [level 2] Title: 피로도, Time: 0.85 ms, Memory: 71.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    
+    static List<Integer> visit;
+    static int answer = 0;
+    
+    public int solution(int k, int[][] dungeons) {
+        visit = new ArrayList<>(Collections.nCopies(dungeons.length, 0));
+        
+        backtrack(k, dungeons, 0);
+        
+        return answer;
+    }
+    
+    public void backtrack(int k, int[][] dungeons, int cnt) {
+
+        if(!visit.contains(0)) {
+            if(cnt > answer) {
+                answer = cnt;
+            }
+            return;
+        }
+        
+        for(int i = 0; i < dungeons.length; i++) {
+            if(visit.get(i) == 0) {
+                visit.set(i, 1);
+                if(k >= dungeons[i][0]) {
+                    backtrack(k - dungeons[i][1], dungeons, cnt + 1);
+                }
+                else {
+                    backtrack(k, dungeons, cnt);
+                }
+                visit.set(i, 0);
+            }
+            
+        }
+        
+        return;
+    }
+    
+}
