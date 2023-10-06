dfcb451687ae980770913c74c7a48a59b4270a9d
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2023-10-06 10:25:46 +0900

    [unrated] Title: 디펜스 게임, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.py
@@ -0,0 +1,15 @@
+import heapq
+
+def solution(n, k, enemy):
+    hq = []
+    
+    for i in range(len(enemy)):        
+        heapq.heappush(hq, enemy[i])
+        
+        if len(hq) > k:
+            n -= heapq.heappop(hq)
+            
+        if n < 0:
+            return i
+        
+    return len(enemy)
