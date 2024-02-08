2848df91dee8230f3159844308a6b3d00c1370b3
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2024-02-08 14:37:11 +0900

    [level 2] Title: 올바른 괄호, Time: 16.75 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,22 +2,25 @@ import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        Stack<Character> st = new Stack<>();
+        boolean answer = true;
+        Stack<Character> stk = new Stack<>();
         
-        for(int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(')
-                st.push('(');
-            else if (s.charAt(i) == ')') {
-                if (st.isEmpty())
-                    return false;
-                else
-                    st.pop();
+        for (char c : s.toCharArray()) {
+            if (c == ')' && stk.isEmpty()) {
+                return false;
+            }
+            
+            else if (c == '(') {
+                stk.push(c);
+            }
+            
+            else {
+                stk.pop();
             }
         }
         
-        if (st.size() == 0)
-            return true;
-        
-        return false;
+        if (!stk.isEmpty()) return false;
+
+        return answer;
     }
 }
