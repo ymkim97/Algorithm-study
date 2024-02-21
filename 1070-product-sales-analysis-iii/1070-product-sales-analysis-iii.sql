2ebf05a822e63fcdbeb7774f29009ba9b853907e
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2024-02-21 17:49:57 +0900

    Time: 1521 ms (86.7%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,6 @@
+select s.product_id, s.year as first_year, s.quantity, s.price
+from sales s
+where (s.product_id, s.year) in 
+    (select product_id, min(year)
+        from sales
+            group by product_id)
