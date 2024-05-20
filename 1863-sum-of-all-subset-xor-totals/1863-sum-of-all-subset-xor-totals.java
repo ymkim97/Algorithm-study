0dee86695e12a87c7423c54b05e7a21b5dc47389
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2024-05-20 12:54:11 +0900

    Time: 16 ms (6.62%), Space: 43.4 MB (13.54%) - LeetHub

---
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    int result = 0;
+    boolean[] visited;
+
+    public int subsetXORSum(int[] nums) {
+        for (int i = 1; i <= nums.length; i++) {
+            visited = new boolean[nums.length];
+            backtrack(nums, i, 0, 0, new ArrayList<>());
+        }
+
+        return result;
+    }
+
+    public void backtrack(int[] nums, int targetLen, int idx, int currentLen, List<Integer> vis) {
+        if (currentLen == targetLen) {
+            int tmp = 0;
+
+            for (int t : vis) {
+                if (tmp == 0) tmp = t;
+                else tmp ^= t;
+            }
+
+            result += tmp;
+
+            return;
+        }
+
+        for (int i = idx; i < nums.length; i++) {
+            if (!visited[i]) {
+                vis.add(nums[i]);
+                visited[i] = true;
+                backtrack(nums, targetLen, i, currentLen + 1, vis);
+                vis.remove(vis.size() - 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
