bb1f4f6ba2cee08aef03bdae16bfc1b51ed82b35
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2024-06-10 15:14:40 +0900

    [Silver V] Title: Dogs and Cats, Time: 280 ms, Memory: 22968 KB, Score: 5 point -BaekjoonHub

---
+++ 백준/Silver/23261. Dogs and Cats/Dogs and Cats.java
@@ -0,0 +1,51 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+	public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int test = 1; test <= T; test++) {
+        	st = new StringTokenizer(br.readLine());
+        	
+        	int N = Integer.parseInt(st.nextToken());
+        	int D = Integer.parseInt(st.nextToken());
+        	int C = Integer.parseInt(st.nextToken());
+        	int M = Integer.parseInt(st.nextToken());
+        	
+        	int dCount = 0;
+        	
+        	char[] animals = br.readLine().toCharArray();
+        	
+        	for (char a : animals) {
+        		if (a == 'D') dCount += 1;
+        	}
+        	
+        	for (int i = 0; i < N; i++) {
+        		if (animals[i] == 'C') {
+        			if (C > 0) C -= 1;
+        			else break;
+        		} else {
+        			if (D > 0) {
+        				dCount -= 1;
+        				D -= 1;
+        				C += M;
+        			} else break;
+        		}
+        	}
+        	
+        	sb.append("Case #").append(test).append(": ");
+        	
+        	if (dCount != 0) sb.append("NO").append("\n");
+        	else sb.append("YES").append("\n");
+        }
+        
+        System.out.println(sb);
+	}
+}
