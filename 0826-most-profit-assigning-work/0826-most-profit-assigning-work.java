4b7d1900da3a073a42a59560d270069470614e09
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2024-06-18 12:31:10 +0900

    Time: 22 ms (49.14%), Space: 46.4 MB (6.63%) - LeetHub

---
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+        int ans = 0;
+        List<int[]> jobs = new ArrayList<>();
+        
+        for (int i = 0; i < difficulty.length; i++) {
+            jobs.add(new int[]{difficulty[i], profit[i]});
+        }
+        
+        Collections.sort(jobs, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(worker);
+        
+        int j = 0;
+        int maxProfit = 0;
+
+        for (int i = 0; i < worker.length; i++) {
+            while (j < jobs.size() && jobs.get(j)[0] <= worker[i]) {
+                maxProfit = Math.max(maxProfit, jobs.get(j)[1]);
+                j++;
+            }
+
+            ans += maxProfit;
+        }
+        
+        return ans;
+    }
+}
