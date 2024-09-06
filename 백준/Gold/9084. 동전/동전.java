b74164ac7fb698ce29047312f5fb23b2a7cc5f53
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2024-09-06 15:41:59 +0900

    [Gold V] Title: 동전, Time: 108 ms, Memory: 14584 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.java
+++ 백준/Gold/9084. 동전/동전.java
@@ -1,37 +1,39 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Main {
- 
-    public static void main(String[] args) throws Exception {
+	
+	public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        
+         
         int T = Integer.parseInt(br.readLine());
         
-        for (int k = 0; k < T; k++) {
-            int N = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine());
-            int[] coins = new int[N];
-            for (int i = 0; i < N; i++) {
-                coins[i] = Integer.parseInt(st.nextToken());
-            }
-            
-            int M = Integer.parseInt(br.readLine());
-            int[] dp = new int[M + 1];
-            dp[0] = 1;
-            
-            for (int i = 0; i < N; i++) {
-                int coin = coins[i];
-                
-                for (int j = 1; j <= M; j++) {
-                    if (j < coin) continue;
-                    
-                    dp[j] = dp[j] + dp[j - coin];
-                }
-            }
-            
-            System.out.println(dp[M]);
+        for (int test = 0; test < T; test++) {
+        	int N = Integer.parseInt(br.readLine());
+        	int[] coins = new int[N];
+        	
+        	st = new StringTokenizer(br.readLine());
+        	
+        	for (int i = 0; i < N; i++) {
+        		coins[i] = Integer.parseInt(st.nextToken());
+        	}
+        	
+        	int M = Integer.parseInt(br.readLine());
+        	int[] dp = new int[M + 1];
+        	dp[0] = 1;
+        	
+        	for (int i = 0; i < N; i++) {
+        		int coin = coins[i];
+        		
+        		for (int j = 1; j <= M; j++) {
+        			if (j < coin) continue;
+        			
+        			dp[j] = dp[j] + dp[j - coin];
+        		}
+        	}
+        	
+        	System.out.println(dp[M]);
         }
-    }
+	}
 }
