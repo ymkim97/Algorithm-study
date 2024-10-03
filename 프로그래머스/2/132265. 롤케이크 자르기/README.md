97669cc968410f47eafda6360f9137c4c8b93343
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2024-10-03 18:10:29 +0900

    [level 2] Title: 롤케이크 자르기, Time: 111.18 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/README.md
+++ 프로그래머스/2/132265. 롤케이크 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 롤케이크 자르기 - 132265 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132265) 
 
 ### 성능 요약
 
-메모리: 192 MB, 시간: 200.91 ms
+메모리: 137 MB, 시간: 111.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 2일 17:41:50
+2024년 10월 03일 18:10:22
 
 ### 문제 설명
 
--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -3,26 +3,34 @@ import java.util.*;
 class Solution {
     public int solution(int[] topping) {
         int answer = 0;
-        Set<Integer> rightSet = new HashSet<>();
-        Map<Integer, Integer> rightMap = new HashMap<>();
-        Set<Integer> leftSet = new HashSet<>();
-
-        for (int t : topping) {
-            rightSet.add(t);
-            rightMap.put(t, rightMap.getOrDefault(t, 0) + 1);
+        
+        int[] fromLeft = new int[topping.length];
+        int[] fromRight = new int[topping.length];
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for (int i = 0; i < topping.length; i++) {
+            if (!set.contains(topping[i])) set.add(topping[i]);
+            
+            fromLeft[i] = set.size();
         }
         
-        for (int t : topping) {
-            leftSet.add(t);
-            rightMap.put(t, rightMap.get(t) - 1);
+        set = new HashSet<>();
+        
+        for (int i = topping.length - 1; i >= 0; i--) {
+            if (!set.contains(topping[i])) set.add(topping[i]);
             
-            if (rightMap.get(t) == 0) {
-                rightSet.remove(t);
-            }
-            
-            if (leftSet.size() == rightSet.size()) answer += 1;
+            fromRight[i] = set.size();
+        }
+        
+        for (int i = 0; i < topping.length - 1; i++) {
+            if (fromLeft[i] == fromRight[i + 1]) answer += 1;
         }
         
         return answer;
     }
 }
+
+// 1 2 2 3 3 4 4 4
+
+// 4 4 4 4 3 3 2 1
