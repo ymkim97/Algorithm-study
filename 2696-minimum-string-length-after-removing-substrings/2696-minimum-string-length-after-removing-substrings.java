ff649027ecdbcac786fecb1713fcf3220540396a
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2024-10-07 16:49:54 +0900

    Time: 9 ms (17.26%), Space: 44.4 MB (27.11%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minLength(String s) {
+        while (s.contains("AB") || s.contains("CD")) {
+            if (s.contains("AB")) {
+                s = s.replace("AB", "");
+            } else if (s.contains("CD")) {
+                s = s.replace("CD", "");
+            } else break;
+        }
+
+        return s.length();
+    }
+}
