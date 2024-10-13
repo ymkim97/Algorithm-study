4fa2683efa2c68ae9ade4a8f21b7071ed94f3945
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2024-10-13 16:59:18 +0900

    [level 2] Title: 호텔 대실, Time: 9.53 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/README.md
+++ 프로그래머스/2/155651. 호텔 대실/README.md
@@ -1,10 +1,10 @@
 # [level 2] 호텔 대실 - 155651 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651) 
 
 ### 성능 요약
 
-메모리: 87.4 MB, 시간: 11.00 ms
+메모리: 87.3 MB, 시간: 9.53 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 26일 14:09:44
+2024년 10월 13일 16:59:15
 
 ### 문제 설명
 
--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -2,28 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] book_time) {
-        int answer = 1;
-        Queue<Integer> pq = new PriorityQueue<>();
+        int answer = 0;
+        
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
         
         Arrays.sort(book_time, (a, b) -> a[0].compareTo(b[0]));
         
-        for (String[] t : book_time) {
-            String[] startStr = t[0].split(":");
-            String[] endStr = t[1].split(":");
+        for (String[] bk : book_time) {
+            String[] start = bk[0].split(":");
+            String[] end = bk[1].split(":");
             
-            int start = Integer.parseInt(startStr[0]) * 60 + Integer.parseInt(startStr[1]);
-            int end = Integer.parseInt(endStr[0]) * 60 + Integer.parseInt(endStr[1]);
+            int startInt = Integer.parseInt(start[0]) * 60 + Integer.parseInt(start[1]);
+            int endInt = Integer.parseInt(end[0]) * 60 + Integer.parseInt(end[1]);
             
-            if (pq.isEmpty()) {
-                pq.offer(end + 10);
-                continue;
-            }
-            
-            if (pq.peek() <= start) {
+            if (!pq.isEmpty() && startInt >= pq.peek() + 10) {
                 pq.poll();
-            }
+            } 
             
-            pq.offer(end + 10);
+            pq.offer(endInt);
+            
             answer = Math.max(answer, pq.size());
         }
         
