2715eb498cd53b3f25ace646e1511fd7a9657a18
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2024-10-13 13:32:45 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 20.39 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 73.6 MB, 시간: 0.28 ms
+메모리: 84.6 MB, 시간: 20.39 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 13:30:25
+2024년 10월 13일 13:32:40
 
 ### 문제 설명
 
--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,37 +1,34 @@
 import java.util.*;
 
 class Solution {
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int bridgeWeight = 0;
-        Queue<Integer> bridge = new LinkedList<>();
+    public int solution(int bridge_length, int maxWeight, int[] truck_weights) {        
+        Queue<Integer> truckQ = new LinkedList<>();
+        Queue<int[]> bridgeQ = new LinkedList<>(); //0: truckNum, 1: sec
         
-        for (int i = 0; i < bridge_length; i++) {
-            bridge.add(0);
-        }
-        
+        int finishedCnt = 0;
+        int currentWeight = 0;
         int time = 0;
-        int truckIndex = 0;
         
-        while (truckIndex < truck_weights.length) {
-            bridgeWeight -= bridge.poll();
-            
-            int truckWeight = truck_weights[truckIndex];
-            
-            if (bridgeWeight + truckWeight <= weight) {
-                bridge.add(truckWeight);
-                bridgeWeight += truckWeight;
-                truckIndex++;
-            }
-            else {
-                bridge.add(0);
-            }
-            
-            time++;
+        for (int t : truck_weights) {
+            truckQ.offer(t);
         }
         
-        while (bridgeWeight > 0) {
-            bridgeWeight -= bridge.poll();
-            time++;
+        while (finishedCnt != truck_weights.length) {
+            time += 1;
+            
+            while (!bridgeQ.isEmpty() && time - bridgeQ.peek()[1] == bridge_length) {
+                int[] t = bridgeQ.poll();
+                finishedCnt += 1;
+                currentWeight -= t[0];
+            }
+                
+            if (!truckQ.isEmpty()) {
+                if (bridgeQ.size() < bridge_length && (currentWeight + truckQ.peek() <= maxWeight)) {
+                    int truck = truckQ.poll();
+                    bridgeQ.offer(new int[] {truck, time});
+                    currentWeight += truck;
+                }
+            }            
         }
         
         return time;
