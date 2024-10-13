f7399714edea467b05187393964e7a5a60b29601
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2024-10-13 14:19:56 +0900

    [level 2] Title: 연속된 부분 수열의 합, Time: 53.32 ms, Memory: 148 MB -BaekjoonHub

--- 프로그래머스/2/178870. 연속된 부분 수열의 합/README.md
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속된 부분 수열의 합 - 178870 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/178870) 
 
 ### 성능 요약
 
-메모리: 72.4 MB, 시간: 0.79 ms
+메모리: 148 MB, 시간: 53.32 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 22일 17:46:00
+2024년 10월 13일 14:19:53
 
 ### 문제 설명
 
--- 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
+++ 프로그래머스/2/178870. 연속된 부분 수열의 합/연속된 부분 수열의 합.java
@@ -2,43 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] sequence, int k) {
-        List<int[]> available = new ArrayList<>();
+        List<int[]> answer = new ArrayList<>();
         
         int left = 0;
         int right = 0;
         int sum = sequence[0];
         
-        while (right < sequence.length) {
-            if (sum == k) {
-                available.add(new int[] {left, right});
-                
-                if (right + 1 == sequence.length) break;
-                
+        while (right < sequence.length) {            
+            if (sum < k) {
                 right += 1;
+                if (right == sequence.length) break;
                 sum += sequence[right];
+            } else if (sum > k) {
                 sum -= sequence[left];
                 left += 1;
-            }
-            
-            else if (sum < k) {
-                if (right + 1 == sequence.length) break;
-                
+            } else {
+                answer.add(new int[] {left, right});
                 right += 1;
+                if (right == sequence.length) break;
                 sum += sequence[right];
-            }
-            
-            else {
-                sum -= sequence[left];
-                left += 1;
             }
         }
         
-        Collections.sort(available, (a, b) -> {
-           if ((a[1] - a[0]) != (b[1] - b[0])) return (a[1] - a[0]) - (b[1] - b[0]);
+        Collections.sort(answer, (a, b) -> {
+            if ((a[1] - a[0]) == (b[1] - b[0])) return a[0] - b[0];
             
-            return 0;
+            return (a[1] - a[0]) - (b[1] - b[0]);
         });
         
-        return available.get(0);
+        return answer.get(0);
     }
 }
