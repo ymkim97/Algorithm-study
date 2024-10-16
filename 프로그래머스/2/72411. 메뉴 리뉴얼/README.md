0ecb85532b459d52f31c5cb0b944c3cbfdd8f49e
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2024-10-16 16:31:08 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 22.08 ms, Memory: 69.9 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/README.md
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/README.md
@@ -1,10 +1,10 @@
 # [level 2] 메뉴 리뉴얼 - 72411 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72411?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72411) 
 
 ### 성능 요약
 
-메모리: 86.3 MB, 시간: 35.20 ms
+메모리: 69.9 MB, 시간: 22.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 25일 16:40:14
+2024년 10월 16일 16:31:13
 
 ### 문제 설명
 
--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,50 +1,54 @@
 import java.util.*;
 
 class Solution {
-    Map<String, Integer> permu = new HashMap<>();
-    
     public String[] solution(String[] orders, int[] course) {
         List<String> answer = new ArrayList<>();
         
-        for (String order : orders) {
-            for (int length : course) {
-                if (order.length() < length) continue;
+        for (int c : course) {
+            HashMap<String, Integer> map = new HashMap<>();
+            
+            for (int i = 0; i < orders.length; i++) {
+                char[] str = orders[i].toCharArray();
+                Arrays.sort(str);
                 
-                getPermus(order, length, -1, "");
-            }
-        }
-
-        List<String> keys = new ArrayList<>(permu.keySet());
-        Collections.sort(keys, (a, b) -> permu.get(b) - permu.get(a));
-        int[] maxLengths = new int[11];
-        
-        for (String key : keys) {
-            if (permu.get(key) >= maxLengths[key.length()]) {
-                maxLengths[key.length()] = permu.get(key);
-                
-                if (permu.get(key) >= 2) {
-                    answer.add(key);
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < str.length; j++) {
+                    sb.append(str[j]);
                 }
+                
+                dfs(0, c, sb.toString(), map, new StringBuilder());
             }
+            
+            List<String> keys = new ArrayList<>(map.keySet());
+            Collections.sort(keys, (a, b) -> map.get(b) - map.get(a));
+            
+            if (keys.isEmpty() || map.get(keys.get(0)) < 2) continue;
+            
+            int maxCnt = map.get(keys.get(0));
+            int idx = 0;
+            
+            while (idx < keys.size() && map.get(keys.get(idx)) == maxCnt) {
+                answer.add(keys.get(idx));
+                idx += 1;
+            }
+            
         }
-    
+        
         Collections.sort(answer);
         
-        return answer.toArray(new String[answer.size()]);
+        return answer.toArray(new String[0]);
     }
     
-    public void getPermus(String order, int targetLength, int idx, String comb) {
-        if (comb.length() == targetLength) {
-            char[] tmp = comb.toCharArray();
-            Arrays.sort(tmp);
-            comb = String.valueOf(tmp);
-            permu.put(comb, permu.getOrDefault(comb, 0) + 1);
+    public void dfs(int start, int length, String order, HashMap<String, Integer> map, StringBuilder sb) {
+        if (length == sb.length()) {
+            map.put(sb.toString(), map.getOrDefault(sb.toString(), 0) + 1);
             return;
         }
         
-        for (int i = idx + 1; i < order.length(); i++) {
-            getPermus(order, targetLength, i, comb + order.charAt(i));
+        for (int i = start; i < order.length(); i++) {
+            sb.append(order.charAt(i));
+            dfs(i + 1, length, order, map, sb);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
-
 }
