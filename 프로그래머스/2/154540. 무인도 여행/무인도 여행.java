a340928b1677d4319936f90e47a37b7f1687e225
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2024-10-18 13:27:56 +0900

    [level 2] Title: 무인도 여행, Time: 51.01 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -2,48 +2,40 @@ import java.util.*;
 
 class Solution {
     
-    int[] mx = {1, -1, 0, 0};
-    int[] my = {0, 0, 1, -1};
-    boolean[][] visited;
-    int n, m;
-    List<Integer> answer = new ArrayList<>();
-    int[][] maps;
+    private static int[] mx = {1, -1, 0, 0};
+    private static int[] my = {0, 0, 1, -1};
+    private int n, m;
     
     public int[] solution(String[] maps) {
         n = maps.length;
         m = maps[0].length();
-        visited = new boolean[n][m];
-        this.maps = new int[n][m];
         
-        for (int i = 0; i < n; i++) {
+        List<Integer> answer = new ArrayList<>();
+        char[][] map = new char[n][m];
+        boolean[][] visited = new boolean[n][m];
+        
+        for (int i = 0;i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (maps[i].charAt(j) != 'X') {
-                    this.maps[i][j] = Character.getNumericValue(maps[i].charAt(j));
-                }
-                else {
-                    this.maps[i][j] = -1;
-                }
+                map[i][j] = maps[i].charAt(j);
             }
         }
         
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (!visited[i][j] && this.maps[i][j] != -1) {
-                    answer.add(bfs(i, j));
-                }
+                if (!visited[i][j] && map[i][j] != 'X') bfs(i, j, map, visited, answer);
             }
         }
         
         Collections.sort(answer);
         
-        return answer.isEmpty() ? new int[] {-1} : answer.stream().mapToInt(a -> a).toArray();
+        return answer.isEmpty() ? new int[] {-1} : answer.stream().mapToInt(Integer::valueOf).toArray();
     }
     
-    public int bfs(int a, int b) {
-        int sum = maps[a][b];
+    public void bfs(int a, int b, char[][] map, boolean[][] visited, List<Integer> answer) {        
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[] {a, b});
         visited[a][b] = true;
+        q.offer(new int[] {a, b});
+        int count = Character.getNumericValue(map[a][b]);
         
         while (!q.isEmpty()) {
             int[] now = q.poll();
@@ -54,14 +46,14 @@ class Solution {
                 int dx = x + mx[i];
                 int dy = y + my[i];
                 
-                if (dx < 0 || dy < 0 || dx >= n || dy >= m || visited[dx][dy] || maps[dx][dy] == -1) continue;
+                if (dx < 0 || dy < 0 || dx >= n || dy >= m || visited[dx][dy] || map[dx][dy] == 'X') continue;
                 
                 visited[dx][dy] = true;
-                sum += maps[dx][dy];
+                count += Character.getNumericValue(map[dx][dy]);
                 q.offer(new int[] {dx, dy});
             }
         }
         
-        return sum;
+        answer.add(count);
     }
 }
