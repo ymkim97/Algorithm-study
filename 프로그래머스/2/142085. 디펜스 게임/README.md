5d97652a8112f5bc61eb0725289c6e776065b5eb
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2024-10-24 13:54:35 +0900

    [level 2] Title: 디펜스 게임, Time: 110.70 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/README.md
+++ 프로그래머스/2/142085. 디펜스 게임/README.md
@@ -1,10 +1,10 @@
 # [level 2] 디펜스 게임 - 142085 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085) 
 
 ### 성능 요약
 
-메모리: 80.6 MB, 시간: 0.92 ms
+메모리: 143 MB, 시간: 110.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 02일 18:33:19
+2024년 10월 24일 13:54:37
 
 ### 문제 설명
 
--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -3,23 +3,23 @@ import java.util.*;
 class Solution {
     public int solution(int n, int k, int[] enemy) {
         int answer = enemy.length;
-        Queue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
-
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
         for (int i = 0; i < enemy.length; i++) {
             n -= enemy[i];
-            pq.add(enemy[i]);
-
+            pq.offer(enemy[i]);
+            
             if (n < 0) {
                 if (k > 0 && !pq.isEmpty()) {
+                    k -= 1;
                     n += pq.poll();
-                    k--;
                 } else {
                     answer = i;
                     break;
                 }
             }
         }
-
+        
         return answer;
     }
 }
