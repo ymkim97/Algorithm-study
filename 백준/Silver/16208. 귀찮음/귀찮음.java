63fa9758a6b9a906fa6d3cdf605e474dfdb9823f
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2024-11-01 12:45:22 +0900

    [Silver V] Title: 귀찮음, Time: 100 ms, Memory: 14136 KB, Score: 4 point -BaekjoonHub

---
+++ 백준/Silver/16208. 귀찮음/귀찮음.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine());
+		
+		int sum = 0;
+		Queue<Integer> pq = new PriorityQueue<>();
+		
+		for (int i = 0; i < N; i++) {
+			int n = Integer.parseInt(st.nextToken());
+			
+			sum += n;
+			pq.offer(n);
+		}
+		
+		int answer = 0;
+		
+		for (int i = 0; i < N - 1; i++) {
+			int cut = pq.poll();
+			sum -= cut;
+			
+			answer += cut * sum;
+		}
+		
+		System.out.println(answer);
+	}
+}
