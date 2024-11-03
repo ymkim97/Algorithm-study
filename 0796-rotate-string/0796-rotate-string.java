2074f47446f720f33d6f2e2bf90e91cd8e43e093
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2024-11-03 22:37:06 +0900

    Time: 0 ms (100%), Space: 41.4 MB (51.17%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) return false;
+
+        StringBuilder sb = new StringBuilder(s);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (sb.toString().equals(goal)) return true;
+
+            char c = sb.charAt(0);
+            sb.deleteCharAt(0);
+            sb.append(c);
+        }
+
+        return false;
+    }
+}
