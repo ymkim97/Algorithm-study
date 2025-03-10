9600756d4d514e4eade235655dc8539a358a4a0a
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2025-03-10 21:18:51 +0900

    Time: 277 ms (42.74%), Space: 46.3 MB (50.43%) - LeetHub

---
+++ 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.java
@@ -0,0 +1,54 @@
+class Solution {
+
+    public long countOfSubstrings(String word, int k) {
+        return atLeastK(word, k) - atLeastK(word, k + 1);
+    }
+
+    private long atLeastK(String word, int k) {
+        long numValidSubstrings = 0;
+        int start = 0;
+        int end = 0;
+
+        HashMap<Character, Integer> vowelCount = new HashMap<>();
+        int consonantCount = 0;
+
+        while (end < word.length()) {
+    
+            char newLetter = word.charAt(end);
+
+            if (isVowel(newLetter)) {
+                vowelCount.put(
+                    newLetter,
+                    vowelCount.getOrDefault(newLetter, 0) + 1
+                );
+            } else {
+                consonantCount++;
+            }
+
+            while (vowelCount.size() == 5 && consonantCount >= k) {
+                numValidSubstrings += word.length() - end;
+                char startLetter = word.charAt(start);
+                if (isVowel(startLetter)) {
+                    vowelCount.put(
+                        startLetter,
+                        vowelCount.get(startLetter) - 1
+                    );
+                    if (vowelCount.get(startLetter) == 0) {
+                        vowelCount.remove(startLetter);
+                    }
+                } else {
+                    consonantCount--;
+                }
+                start++;
+            }
+
+            end++;
+        }
+
+        return numValidSubstrings;
+    }
+
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+}
