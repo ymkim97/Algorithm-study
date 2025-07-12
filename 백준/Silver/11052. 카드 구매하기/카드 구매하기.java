a04f5f5a495401552a550fbc525df99e8c555a1e
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2025-07-12 11:31:27 +0900

    [Silver I] Title: 카드 구매하기, Time: 132 ms, Memory: 14448 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -6,12 +6,11 @@ class Main {
 	public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-     
+        
         int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
         
         int[] cards = new int[N + 1];
-        
-        st = new StringTokenizer(br.readLine());
         
         for (int i = 1; i <= N; i++) {
         	cards[i] = Integer.parseInt(st.nextToken());
