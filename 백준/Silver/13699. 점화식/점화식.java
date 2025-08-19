e261dc2155cd956d4495f58e8d348cb594e0a9ff
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2025-08-19 16:00:43 +0900

    [Silver IV] Title: 점화식, Time: 116 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Silver/13699. 점화식/점화식.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        long[] dp = new long[36];
+
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
