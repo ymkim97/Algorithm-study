ca218f0e1d8bd193c803de33e8c7be862919f0b5
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2025-09-10 16:47:58 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 235.69 ms, Memory: 96 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -1,18 +1,53 @@
+import java.util.*;
+
 class Solution {
     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
         long answer = 0;
-
-        int d = 0;
-        int p = 0;
         
-        for(int i = n - 1; i >= 0; i--){
-            d -= deliveries[i];
-            p -= pickups[i];
+        Stack<int[]> delivery = new Stack<>(); // 0: house, 1: amount
+        Stack<int[]> pickup = new Stack<>();
+        
+        for (int i = 0; i < n; i++) {
+            if (deliveries[i] != 0) {
+                delivery.push(new int[] {i + 1, deliveries[i]});
+            }
             
-            while(d < 0 || p < 0){
-                d += cap;
-                p += cap;
-                answer += (i + 1) * 2;
+            if (pickups[i] != 0) {
+                pickup.push(new int[] {i + 1, pickups[i]});
+            }
+        }
+        
+        while (!delivery.isEmpty() || !pickup.isEmpty()) {
+            int delCount = 0;
+            int picCount = 0;
+            
+            int dHouse = delivery.isEmpty() ? 0 : delivery.peek()[0];
+            int pHouse = pickup.isEmpty() ? 0 : pickup.peek()[0];
+            
+            answer += Math.max(dHouse, pHouse) * 2;
+            
+            while (!delivery.isEmpty() && delCount < cap) {
+                if (delivery.peek()[1] + delCount <= cap) {
+                    int[] d = delivery.pop();
+                    delCount += d[1];
+                } else if (delivery.peek()[1] + delCount > cap) {
+                    int[] d = delivery.pop();
+                    d[1] -= cap - delCount;
+                    delivery.push(d);
+                    break;
+                }
+            }
+            
+            while (!pickup.isEmpty() && picCount < cap) {
+                if (pickup.peek()[1] + picCount <= cap) {
+                    int[] d = pickup.pop();
+                    picCount += d[1];
+                } else if (pickup.peek()[1] + picCount > cap) {
+                    int[] d = pickup.pop();
+                    d[1] -= cap - picCount;
+                    pickup.push(d);
+                    break;
+                }
             }
         }
         
