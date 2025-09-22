3e70ca2c5512e2c3a2d2f433c0dbaf56e3a48e03
Youngmyung Kim <83266154+ymkim97@users.noreply.github.com>
2025-09-22 17:51:26 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 50.12 ms, Memory: 212 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -1,6 +1,54 @@
 class Solution {
     public int solution(int[][] board, int[][] skill) {
         int answer = 0;
+        
+        int N = board.length;
+        int M = board[0].length;
+        
+        int[][] prefixSum = new int[N + 1][M + 1];
+        
+        for (int[] sk : skill) {
+            int type = sk[0];
+            int r1 = sk[1];
+            int c1 = sk[2];
+            int r2 = sk[3];
+            int c2 = sk[4];
+            int degree = sk[5];
+            
+            if (type == 1) {
+                prefixSum[r1][c1] -= degree;
+                prefixSum[r1][c2 + 1] += degree;
+                prefixSum[r2 + 1][c1] += degree;
+                prefixSum[r2 + 1][c2 + 1] -= degree;
+            } else {
+                prefixSum[r1][c1] += degree;
+                prefixSum[r1][c2 + 1] -= degree;
+                prefixSum[r2 + 1][c1] -= degree;
+                prefixSum[r2 + 1][c2 + 1] += degree;
+            }
+        }
+        
+        // left to right
+        for (int i = 0; i <= N; i++) {
+            for (int j = 1; j <= M; j++) {
+                prefixSum[i][j] += prefixSum[i][j - 1];
+            }   
+        }
+        
+        // top to bottom
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j <= M; j++) {
+                prefixSum[i][j] += prefixSum[i - 1][j];
+            }   
+        }
+        
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                board[i][j] += prefixSum[i][j];
+                if (board[i][j] > 0) answer += 1;
+            }
+        }
+        
         return answer;
     }
 }
